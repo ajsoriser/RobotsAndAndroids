e4c22850b545641c0b8be77c926d96f6769f00b7
ajsoriser <ajsoriser@gmail.com>
2017-12-11 13:19:17 -0500

    Homework Hello World

---
+++ app/src/main/java/com/example/ajsor/myfirstnewapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ajsor.myfirstnewapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Display();
+    }
+
+    public void Display(){
+
+        Button cmdEnter = findViewById(R.id.btnEnter);
+        final EditText txtInput = findViewById(R.id.txtInput);
+        final TextView lblDisplay = findViewById(R.id.lblDisplay);
+
+        cmdEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Aaron;
+                Aaron = "Hello, " + txtInput.getText().toString();
+                lblDisplay.setText(Aaron);
+            }
+        });
+    }
+}
+
